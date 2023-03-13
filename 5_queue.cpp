a275576466f5ce50a9c0fa6388c24adabb74715c
Chavan Saurabh Ravindra <82364105+saurabh0033@users.noreply.github.com>
2023-03-13 09:49:43 +0530

    Update 5_queue.cpp

--- 5_queue.cpp
+++ 5_queue.cpp
@@ -1,6 +1,7 @@
 
 /*
-Queues are a type of container adaptors that operate in a first in first out (FIFO) type of arrangement. Elements are inserted at the back (end) and are deleted from the front. 
+Queues are a type of container adaptors that operate in a first in first out (FIFO) type of arrangement. 
+Elements are inserted at the back (end) and are deleted from the front. 
 */
 #include<bits/stdc++.h>
 using namespace std;
