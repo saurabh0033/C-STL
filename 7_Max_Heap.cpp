42f7f5da90728ba34f6ed9bd85789dcf44bd6b7f
Chavan Saurabh Ravindra <82364105+saurabh0033@users.noreply.github.com>
2023-03-15 18:16:34 +0530

    Create 7_Max_Heap.cpp

---
+++ 7_Max_Heap.cpp
@@ -0,0 +1,27 @@
+
+//in max heap, elements are stored in non-decreasing order;
+// hence it is known as Max Heap
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    priority_queue<int>p;
+    int size;
+    cout<<"Enter the size-";
+    cin>>size;
+    for(int i=0;i<size;i++){
+        int n;cin>>n;
+        p.push(n);
+    }
+    
+    //===============================================================
+    cout<<"Elements in max Heap"<<endl;
+    while(!p.empty()){
+        cout<<p.top()<<" ";
+        p.pop();
+    }
+    // Elements in max Heap
+    // 5 4 3 2 1 
+    return 0;
+}
